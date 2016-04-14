41a6750bc1b7bfdb663916eb71696e58bc2a7799
hdennen <hdennen@users.noreply.github.com>
2016-04-14 18:07:14 +0200

    port:80

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var nicknames = [];
 
-server.listen(3000);
+server.listen(80);
 
 app.get('/', function(req, res){ //send the home page
 	res.sendFile(__dirname +'/index.html');
